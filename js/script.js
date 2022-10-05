addda6deceb3027f68b03e1553d3e99483649c4e
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-05 13:42:23 +0000

    Added a function

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
+//
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor2
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
